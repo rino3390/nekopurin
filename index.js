97e7f37a0bdc287176d0234abdf041262b0db01f
rino3390 <sherikawaii3390@gmail.com>
2021-07-27 04:18:58 +0800

    feat: add port
    解決heroku一直自行error的問題

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ require('dotenv').config()
 const Index = require('discord.js');
 const token = process.env["DCTOKEN"];
 const client = new Index.Client();
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+	var result = 'App is running'
+	response.send(result);
+}).listen(app.get('port'), function() {
+	console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 client.login(token);
 
